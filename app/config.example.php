b07755c7909506cbe38983cbd386aaee198190de
Cristian Castro <130397069+Cribas14@users.noreply.github.com>
2025-08-12 13:14:39 -0400

    Create config.example.php

---
+++ app/config.example.php
@@ -0,0 +1,13 @@
+<?php
+return [
+  'db' => [
+    'host'    => '127.0.0.1',
+    'port'    => '3306',
+    'name'    => 'megamedia_news',
+    'user'    => 'root',
+    'pass'    => '',
+    'charset' => 'utf8mb4',
+  ],
+  'base_url' => 'http://localhost/megamedia_newsDuoc',
+  'ga4_id'   => 'G-XXXXXXXXXX' // reemplazar en local
+];
